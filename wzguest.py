fabeac40c99cfe72ce16757bfe47db5ccbe7b63a
iero <greg@digital-gf.local>
2017-05-09 08:51:15 +0200

    Handle error

--- wzguest.py
+++ wzguest.py
@@ -9,12 +9,17 @@ from urllib.parse import urlparse
 
 def getMagic(client, url) :
     print("+-[url] : {}".format(url))
-    r = client.get(url)
-    soup = BeautifulSoup(r.content, "html.parser")
-    #print(soup)
-    desc = soup.find("input", {"name":"magic"})
-    magic = desc['value']
-    print("+-[magic] : {}".format(magic))
+    magic = None
+    try :
+        r = client.get(url)
+        soup = BeautifulSoup(r.content, "html.parser")
+        #print(soup)
+        desc = soup.find("input", {"name":"magic"})
+        magic = desc['value']
+        print("+-[magic] : {}".format(magic))
+    except :
+        print("+-[magic] Cannot reach internet. Please wait")
+
     return magic
 
 def letMeIn(client, baseurl, magic, username, password,redirurl) :
@@ -27,7 +32,7 @@ def letMeIn(client, baseurl, magic, username, password,redirurl) :
     url = baseurl+"/fgtauth?"+magic
     print("+-[url] : {}".format(url))
     r = client.post(url, data=payload, headers=dict(Referer=url))
-    print("+-[status] : {} (need to be 200)".format(r.status))
+    #print("+-[status] : {} (need to be 200)".format(r.status))
 
     # Send credits
     payload = urllib.parse.urlencode({
@@ -39,7 +44,7 @@ def letMeIn(client, baseurl, magic, username, password,redirurl) :
     url = baseurl+"/"
     print("+-[url] : {}".format(url))
     r = client.post(url, data=payload, headers=dict(Referer=url))
-    print("+-[status] : {} (need to be 303)".format(r.status))
+    #print("+-[status] : {} (need to be 303)".format(r.status))
 
 if __name__ == "__main__":
 
@@ -55,4 +60,5 @@ if __name__ == "__main__":
 
     client = requests.session()
     magic = getMagic(client,redirurl)
-    letMeIn(client,url,magic,username,password,redirurl)
+    if magic :
+        letMeIn(client,url,magic,username,password,redirurl)
